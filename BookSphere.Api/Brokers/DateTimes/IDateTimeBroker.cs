d9423ca3d62078b9c2db2855ce8cad63f638696a
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-05-04 16:30:34 +0500

    BROKERS: Implement DateTime

---
+++ BookSphere.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using System;
+
+namespace BookSphere.Api.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTimeOffset();
+    }
+}
