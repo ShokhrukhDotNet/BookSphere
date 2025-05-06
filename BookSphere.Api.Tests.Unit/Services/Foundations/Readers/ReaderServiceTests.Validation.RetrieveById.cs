234434e0acb999b7df79f4449ba269e613348df1
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-05-06 17:47:46 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,55 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit.Abstractions;
+using BookSphere.Api.Models.Foundations.Readers.Exceptions;
+using BookSphere.Api.Models.Foundations.Readers;
+using FluentAssertions;
+
+namespace BookSphere.Api.Tests.Unit.Services.Foundations.Readers
+{
+    public partial class ReaderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidReaderId = Guid.Empty;
+            var invalidReaderException = new InvalidReaderException();
+
+            invalidReaderException.AddData(
+                key: nameof(Reader.Id),
+                values: "Id is required");
+
+            var expectedReaderValidationException =
+                new ReaderValidationException(invalidReaderException);
+
+            // when
+            ValueTask<Reader> retrieveReaderById =
+                this.readerService.RetrieveReaderByIdAsync(invalidReaderId);
+
+            ReaderValidationException actualReaderValidationException =
+                await Assert.ThrowsAsync<ReaderValidationException>(retrieveReaderById.AsTask);
+
+            // then
+            actualReaderValidationException.Should()
+                .BeEquivalentTo(expectedReaderValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedReaderValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReaderByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
