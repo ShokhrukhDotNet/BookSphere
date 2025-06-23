85868c4f3b6613458049df319fd24d4f6c89c47f
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-23 21:03:49 +0500

    DATA: Model & Migrate Books

---
+++ BookSphere.Api/Models/Foundations/Books/Book.cs
@@ -0,0 +1,22 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using System;
+using System.Text.Json.Serialization;
+using BookSphere.Api.Models.Foundations.Readers;
+
+namespace BookSphere.Api.Models.Foundations.Books
+{
+    public class Book
+    {
+        public Guid BookId { get; set; }
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public Guid ReaderId { get; set; }
+        [JsonIgnore]
+        public Reader? Reader { get; set; }
+    }
+}
