33b980a659b163f6902f44b1b69cec54c263240e
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-24 00:29:15 +0500

    ShouldThrowValidationExceptionOnAddIfBookIsInvalidAndLogItAsync -> FAIL

--- BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Validation.Add.cs
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Validation.Add.cs
@@ -42,5 +42,64 @@ namespace BookSphere.Api.Tests.Unit.Services.Foundations.Books
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfBookIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidBook = new Book
+            {
+                BookTitle = invalidText
+            };
+
+            var invalidBookException = new InvalidBookException();
+
+            invalidBookException.AddData(
+                key: nameof(Book.BookId),
+                values: "Id is required");
+
+            invalidBookException.AddData(
+                key: nameof(Book.BookTitle),
+                values: "Text is required");
+
+            invalidBookException.AddData(
+                key: nameof(Book.Author),
+                values: "Text is required");
+
+            invalidBookException.AddData(
+                key: nameof(Book.Genre),
+                values: "Text is required");
+
+            invalidBookException.AddData(
+                key: nameof(Book.ReaderId),
+                values: "Id is required");
+
+            var expectedBookValidationException =
+                new BookValidationException(invalidBookException);
+
+            // when
+            ValueTask<Book> addBookTask =
+                this.bookService.AddBookAsync(invalidBook);
+
+            // then
+            await Assert.ThrowsAsync<BookValidationException>(() =>
+                addBookTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedBookValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertBookAsync(It.IsAny<Book>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
