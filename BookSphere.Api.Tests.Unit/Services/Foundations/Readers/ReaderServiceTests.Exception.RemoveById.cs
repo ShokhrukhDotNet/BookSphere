4d80ed035e56af727de274fe228406885cd99071
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-23 17:02:45 +0500

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> FAIL

--- BookSphere.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exception.RemoveById.cs
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exception.RemoveById.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using BookSphere.Api.Models.Foundations.Readers;
 using BookSphere.Api.Models.Foundations.Readers.Exceptions;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 
@@ -53,6 +54,49 @@ namespace BookSphere.Api.Tests.Unit.Services.Foundations.Readers
 
             this.storageBrokerMock.Verify(broker =>
                 broker.DeleteReaderAsync(It.IsAny<Reader>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someLocationId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedReaderStorageException =
+                new FailedReaderStorageException(sqlException);
+
+            var expectedReaderDependencyException =
+                new ReaderDependencyException(failedReaderStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectReaderByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Reader> deleteReaderTask =
+                this.readerService.RemoveReaderByIdAsync(someLocationId);
+
+            var actualReaderDependencyException =
+                await Assert.ThrowsAsync<ReaderDependencyException>(
+                    deleteReaderTask.AsTask);
+
+            // then
+            actualReaderDependencyException.Should()
+                .BeEquivalentTo(expectedReaderDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReaderByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedReaderDependencyException))),
+                        Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
