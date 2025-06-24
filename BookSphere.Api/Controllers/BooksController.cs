864aa1cd5877de7d554079b0cc5ddc0d560d6c24
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-24 14:15:30 +0500

    EXPOSERS: Post Book

---
+++ BookSphere.Api/Controllers/BooksController.cs
@@ -0,0 +1,58 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using System.Threading.Tasks;
+using BookSphere.Api.Models.Foundations.Books;
+using BookSphere.Api.Models.Foundations.Books.Exceptions;
+using BookSphere.Api.Services.Foundations.Books;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace BookSphere.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : RESTFulController
+    {
+        private readonly IBookService bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Book>> PostBookAsync(Book book)
+        {
+            try
+            {
+                Book postedBook = await this.bookService.AddBookAsync(book);
+
+                return Created(postedBook);
+            }
+            catch (BookValidationException bookValidationException)
+            {
+                return BadRequest(bookValidationException.InnerException);
+            }
+            catch (BookDependencyValidationException bookDependencyValidationException)
+                when (bookDependencyValidationException.InnerException is AlreadyExistBookException)
+            {
+                return Conflict(bookDependencyValidationException.InnerException);
+            }
+            catch (BookDependencyValidationException bookDependencyValidationException)
+            {
+                return BadRequest(bookDependencyValidationException.InnerException);
+            }
+            catch (BookDependencyException bookDependencyException)
+            {
+                return InternalServerError(bookDependencyException.InnerException);
+            }
+            catch (BookServiceException bookServiceException)
+            {
+                return InternalServerError(bookServiceException.InnerException);
+            }
+        }
+    }
+}
