1af13591d7d7cf735b4aee3c907e6a5262436de1
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-25 09:35:22 +0500

    BROKERS: Select Book

--- BookSphere.Api/Brokers/Storages/StorageBroker.Books.cs
+++ BookSphere.Api/Brokers/Storages/StorageBroker.Books.cs
@@ -3,6 +3,8 @@
 // Free To Use To Bridge Knowledge and Curiosity
 //==================================================
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BookSphere.Api.Models.Foundations.Books;
 using Microsoft.EntityFrameworkCore;
@@ -15,5 +17,10 @@ namespace BookSphere.Api.Brokers.Storages
 
         public async ValueTask<Book> InsertBookAsync(Book book) =>
             await InsertAsync(book);
+
+        public IQueryable<Book> SelectAllBooks() => SelectAll<Book>();
+
+        public async ValueTask<Book> SelectBookByIdAsync(Guid bookId) =>
+            await SelectAsync<Book>(bookId);
     }
 }
