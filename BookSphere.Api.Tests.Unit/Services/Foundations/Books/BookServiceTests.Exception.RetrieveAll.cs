a8de661a2d46af04392352b3031cb97530606572
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-25 11:56:19 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exception.RetrieveAll.cs
@@ -0,0 +1,54 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using System;
+using BookSphere.Api.Models.Foundations.Books.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace BookSphere.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedBookStorageException =
+                new FailedBookStorageException(sqlException);
+
+            var expectedBookDependencyException =
+                new BookDependencyException(failedBookStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllBooks()).Throws(sqlException);
+
+            // when
+            Action retrieveAllBooksAction = () =>
+                this.bookService.RetrieveAllBooks();
+
+            BookDependencyException actualBookDependencyException =
+                Assert.Throws<BookDependencyException>(retrieveAllBooksAction);
+
+            // then
+            actualBookDependencyException.Should()
+                .BeEquivalentTo(expectedBookDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllBooks(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedBookDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
