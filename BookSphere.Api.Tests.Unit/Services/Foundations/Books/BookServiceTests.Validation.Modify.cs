d79bf9729c04212821c7ad3fbf982a976b2296fe
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-26 12:04:27 +0500

    ShouldThrowValidationExceptionOnModifyIfBookIsNullAndLogItAsync -> FAIL

---
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Validation.Modify.cs
@@ -0,0 +1,50 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Bridge Knowledge and Curiosity
+//==================================================
+
+using System.Threading.Tasks;
+using BookSphere.Api.Models.Foundations.Books;
+using BookSphere.Api.Models.Foundations.Books.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace BookSphere.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfBookIsNullAndLogItAsync()
+        {
+            // given
+            Book nullBook = null;
+            var nullBookException = new NullBookException();
+
+            var expectedBookValidationException =
+                new BookValidationException(nullBookException);
+
+            // when
+            ValueTask<Book> modifyBookTask =
+                this.bookService.ModifyBookAsync(nullBook);
+
+            BookValidationException actualBookValidationException =
+                await Assert.ThrowsAsync<BookValidationException>(
+                    modifyBookTask.AsTask);
+
+            // then
+            actualBookValidationException.Should()
+                .BeEquivalentTo(expectedBookValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedBookValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateBookAsync(It.IsAny<Book>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
