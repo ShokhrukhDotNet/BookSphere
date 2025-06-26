7c0a9f8e7ef0c00836a2bc87a4492d95eccd7dd3
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-06-26 14:02:31 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exception.Modify.cs
+++ BookSphere.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exception.Modify.cs
@@ -10,6 +10,7 @@ using BookSphere.Api.Models.Foundations.Books;
 using BookSphere.Api.Models.Foundations.Books.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace BookSphere.Api.Tests.Unit.Services.Foundations.Books
@@ -48,6 +49,51 @@ namespace BookSphere.Api.Tests.Unit.Services.Foundations.Books
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedBookDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(bookId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateBookAsync(someBook), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            Book randomBook = CreateRandomBook();
+            Book someBook = randomBook;
+            Guid bookId = someBook.BookId;
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedBookStorageException =
+                new FailedBookStorageException(databaseUpdateException);
+
+            var expectedBookDependencyException =
+                new BookDependencyException(failedBookStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(bookId)).Throws(databaseUpdateException);
+
+            // when
+            ValueTask<Book> modifyBookTask =
+                this.bookService.ModifyBookAsync(someBook);
+
+            BookDependencyException actualBookDependencyException =
+                await Assert.ThrowsAsync<BookDependencyException>(
+                    modifyBookTask.AsTask);
+
+            // then
+            actualBookDependencyException.Should()
+                .BeEquivalentTo(expectedBookDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
                     expectedBookDependencyException))), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
